214a69c73a0a63f26107c81c69e8e0f83bbd6de2
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-22 17:56:46 -0400

    Adding my last projects..

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,15 +5,60 @@ export default function Projects() {
   return (
     <div className='outerProjectDiv'>
       <div className='titleProjects'>Projects</div>
+      {/* First Project */}
       <div className='projectsContainer'>
         <img
           className='projectURLImage'
           src={require('../images/instantCart.gif')}
           alt='petcare website'
         />
-        <div className='projectContainer'>
+        <div className='projectDescriptionContainer'>
           <div className='projectTitle'>InstantCart</div>
-          <div className='project Description'></div>
+          <div className='project-Description'>
+            A team effort to recreate the Instacart desktop and mobile application. Instacart is a
+            grocery delivery and pick-up service. The team consists of 5 software engineers and 3
+            user experience (UX) designers. The developers and designers of this recreation are in
+            no way affiliated with Instacart or the product development teams of its partnered
+            grocery stores.
+          </div>
+          <div className='projectLinks'></div>
+        </div>
+      </div>
+      {/* Second Project */}
+      <div className='projectsContainer'>
+        <img
+          className='projectURLImage'
+          src={require('../images/pet_care.png')}
+          alt='pet-care-website'
+        />
+        <div className='projectDescriptionContainer'>
+          <div className='projectTitle'>Pet Care</div>
+          <div className='project-Description'>
+            A team effort to recreate the Instacart desktop and mobile application. Instacart is a
+            grocery delivery and pick-up service. The team consists of 5 software engineers and 3
+            user experience (UX) designers. The developers and designers of this recreation are in
+            no way affiliated with Instacart or the product development teams of its partnered
+            grocery stores.
+          </div>
+          <div className='projectLinks'></div>
+        </div>
+      </div>
+      {/* third Project */}
+      <div className='projectsContainer'>
+        <img
+          className='projectURLImage'
+          src={require('../images/eye4words.png')}
+          alt='wyw4words website'
+        />
+        <div className='projectDescriptionContainer'>
+          <div className='projectTitle'>Eye4Words</div>
+          <div className='project-Description'>
+            A team effort to recreate the Instacart desktop and mobile application. Instacart is a
+            grocery delivery and pick-up service. The team consists of 5 software engineers and 3
+            user experience (UX) designers. The developers and designers of this recreation are in
+            no way affiliated with Instacart or the product development teams of its partnered
+            grocery stores.
+          </div>
           <div className='projectLinks'></div>
         </div>
       </div>
