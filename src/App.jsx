90c46c04a8fb68515f4cf5c4358c6a23d9387752
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-24 03:10:01 -0400

    playing with about Me page again better css

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
     <div className='App'>
       <Header />
       <MainPage />
-      <Projects />
-      <ContactForm />
+      {/* <Projects /> */}
+      {/* <ContactForm /> */}
       <Footer />
     </div>
   );
