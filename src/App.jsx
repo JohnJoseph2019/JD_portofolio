59492cb72acd11e12d79550fe86be1c869a6b2ba
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-24 09:12:46 -0400

    Finish the css for responsive width sizes from desktop to iphone 375px .... for header and MainPage(about me page ) component

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className='App'>
       <Header />
       <MainPage />
-      {/* <Projects /> */}
+      <Projects />
       {/* <ContactForm /> */}
       <Footer />
     </div>
