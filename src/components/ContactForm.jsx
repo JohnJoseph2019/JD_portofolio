49d6c2b13a13fd6917f8672d8417aea38f429922
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-25 17:42:57 -0400

    Completed the css styling for the contactMe component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,19 +3,19 @@ import './ContactForm.css';
 
 export default function contactForm() {
   return (
-    <>
-      <div className='contactMetitle' id='contactMe'>
+    <div className='contactMeContainer'>
+      <div className='contactMeTitle' id='contactMe'>
         Contact Me
       </div>
       <div className='outerContactMeForm'>
         <div className='contactDescription'>
-          Thank You for visiting if you want more inforamtion you can visit my LinkeIn or my github.
+          For more information you can visit my LinkedIn or my github.
           <br />
         </div>
         <div className='contactEmail'>
           You can also reach me at my e-mail <a href='mailto:juan.del89@gmail.com'>Email</a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
