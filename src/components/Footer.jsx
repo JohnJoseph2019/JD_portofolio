bd221c0ac44b0b67f33e222433ba144a79471c3b
Juan Delgado <Delgado-juan@hotmail.com>
2020-07-15 21:35:12 -0400

    added more colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className='outerFooterMain'>
       <a href='https://github.com/JohnJoseph2019' className='gitHub'>
-        <img src={require('../images/GitHub-Mark-64px.png')} alt='github' />
+        <img src={require('../images/Octocat.png')} alt='github' />
       </a>
       <a href='https://www.linkedin.com/in/jj-delgado/' className='linkedIn'>
         <img src={require('../images/linkedin.png')} alt='linkedIn' />
