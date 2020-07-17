4e785120f78e6d8dc337d32443d26f49c7ed3bd6
Juan Delgado <Delgado-juan@hotmail.com>
2020-07-17 19:51:32 -0400

    adding some padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,21 @@ import DropDown from '../shared/DropDown';
 
 export default function Header() {
   return (
-    <div className='outerNavBar'>
-      <div className='navName'>Juan Delgado</div>
-      <DropDown />
-      <div className='contentLinks'>
-        <a href='#aboutMe' className='headerCSS'>
-          About Me
-        </a>
-        <a href='#projects' className='headerCSS'>
-          Projects
-        </a>
-        <a href='#contactMe' className='headerCSS'>
-          Contact Me
-        </a>
+    <div className='aaaaaa'>
+      <div className='outerNavBar'>
+        <div className='navName'>Juan Delgado</div>
+        <DropDown />
+        <div className='contentLinks'>
+          <a href='#aboutMe' className='headerCSS'>
+            About Me
+          </a>
+          <a href='#projects' className='headerCSS'>
+            Projects
+          </a>
+          <a href='#contactMe' className='headerCSS'>
+            Contact Me
+          </a>
+        </div>
       </div>
     </div>
   );
